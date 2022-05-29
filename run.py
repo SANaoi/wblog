2112c7f38d3e3cdc04b385855718b5b3dc70375e
SANaoi <3144347657@qq.com>
2022-05-29 13:55:31 +0800

    Initial commit

---
+++ run.py
@@ -0,0 +1,9 @@
+a = [1,2,3,4,5,6]
+b = [5,6,1,2,9,8]
+c = ['a',1,4,'b']
+
+new = set(a).intersection(b,c)
+
+all_list = [[1, 2, 3, 4, 5, 6], [5, 6, 1, 2, 9, 8], ['a', 1, 4, 'b', 2],[1,2]]
+p = set.intersection(*map(set, all_list))
+print(p)
